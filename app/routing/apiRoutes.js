959a1902ee35849036650b208b3822aeb3661bdf
RPilatti <patrickpilatti1@gmail.com>
2018-02-17 07:03:43 -0800

    semi final commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,47 @@
+var friendData = require('../data/friends.js');
+
+var path = require('path');
+
+var sumDifference = 0;
+
+module.exports = function(app) {
+	app.get('/data/friends.js', function(req, res){
+		res.json("./friends.js");
+	});
+
+	app.post('/data/friends.js', function(req, res){
+		var itsaMatch = {
+		name: "",
+		image: "",
+		matchDifference:1000
+	};
+	var dataInput = req.body;
+	var nameInput = dataInput.name;
+	var imgInput = dataInput.image
+	var scoreInput = dataInput.scores
+
+	var sumDifference = 0;
+
+for(var i = 0; i < [friends].length-1; i++) {
+	console.log(friends[i].name);
+	sumDifference = 0;
+
+
+	for(var j = 0; j < 10; j++) {
+
+		sumDifference += Math.abs(parseInt(scoreInput[j]) - parseInt(friends[i].scores[j]));
+		if (sumDifference <= itsaMatch.friendDifference){
+
+			itsaMatch.name = friends[i].name;
+					itsaMatch.photo = friends[i].photo;
+					itsaMatch.matchDifference = sumDifference;
+					
+		}
+	}
+}
+friends.push(dataInput);
+ 
+		res.json(itsaMatch);
+
+	});
+};
